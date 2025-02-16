43cb157fae50d5ed269c84802a46ad9ad47f6534
Mohd Faziel <mohdfazel969@gmail.com>
2025-02-16 18:51:54 +0530

    learned routing and how to send different response from different routes

--- Express/index.js
+++ Express/index.js
@@ -5,10 +5,31 @@ app.listen(port,()=>
 {
     console.log(`Server is running on port ${port}`);
 });
-app.use((req,res)=>
+// app.use((req,res)=>
+// {
+//     console.log("Request is coming");
+//     res.send(
+//         "<div><h1>Express</h1><p>Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.</p></div>"
+//     );
+// })
+
+app.get("/",(req,res)=>
 {
-    console.log("Request is coming");
-    res.send(
-        "<div><h1>Express</h1><p>Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.</p></div>"
-    );
+    res.send("You are at home page");
 })
+
+app.get("/about",(req,res)=>{
+    res.send("You are at about page");
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("You are at contact page");
+})
+
+app.get("*",(req,res)=>{
+    res.send("Enter valid route");
+})
+
+app.post("/login",(req,res)=>{
+    res.send("Login Successfully");
+});
